c5999171e3cb762b4e44217c90a0c184a83cb5cb
antonvid <antonyvidler@gmail.com>
2023-01-16 04:01:52 +0000

    v0.3

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function hide(id) {
+    var enlarged = document.getElementsByClassName('enlarge');
+    if(enlarged.length>0){
+        return;
+    }
     document.getElementById(id).classList.toggle('enlarge');
+    console.log('toggle');
 }
